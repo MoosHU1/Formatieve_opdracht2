244f8abb74385d5bdf81009885576ec093ddff4c
MoosHU1 <moos.boot@student.hu.nl>
2020-03-07 12:47:51 +0100

    Script om alle tables aan te maken

---
+++ Formatieve_Opdracht2/create_tables.py
@@ -0,0 +1,52 @@
+import psycopg2
+from psycopg2.extensions import ISOLATION_LEVEL_AUTOCOMMIT
+
+# Connect to PostgreSQL DBMS
+con = psycopg2.connect("dbname = voordeelshop user=postgres password=''")
+con.set_isolation_level(ISOLATION_LEVEL_AUTOCOMMIT)
+
+# Obtain a DB Cursor
+cursor = con.cursor()
+
+def create_tables():
+    commands = (
+        '''
+        CREATE TABLE Product
+        (
+          ProductID varchar,
+          GenderID  varchar,
+          ProductName  varchar,
+          Price numeric,
+          PRIMARY KEY (ProductID)	
+        )
+        ''',
+        '''
+        CREATE TABLE Profile
+        (
+          BrowserID varchar,
+          PRIMARY KEY (BrowserID)
+        )
+        ''',
+        '''
+        CREATE TABLE Session_
+        (
+          SessionID varchar,
+          BrowserID varchar,
+          PRIMARY KEY (SessionID),
+          FOREIGN KEY (BrowserID) REFERENCES profile(BrowserID)
+        )
+        ''')
+
+    try:
+        for command in commands:
+            cursor.execute(command)
+
+        cursor.close()
+        con.commit()
+
+    except (Exception, psycopg2.DatabaseError) as error:
+        print(error)
+    finally:
+        if con is not None:
+            con.close()
+create_tables()
