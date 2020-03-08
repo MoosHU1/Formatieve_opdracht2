fed6aa91b4eea52106bb0fc1770fe11093e897af
MoosHU1 <moos.boot@student.hu.nl>
2020-03-08 16:46:01 +0100

    script om alle records te verwijderen

---
+++ Formatieve_Opdracht2/delete_all_records.py
@@ -0,0 +1,34 @@
+import psycopg2
+from psycopg2.extensions import ISOLATION_LEVEL_AUTOCOMMIT
+
+# Connect to PostgreSQL DBMS
+con = psycopg2.connect("dbname = voordeelshop user=postgres password=''")
+con.set_isolation_level(ISOLATION_LEVEL_AUTOCOMMIT)
+
+# Obtain a DB Cursor
+cursor = con.cursor()
+
+
+def delete_records():
+    commands = (
+        """DELETE FROM product""",
+        """DELETE FROM profile""",
+        """DELETE FROM session_"""
+        )
+
+    try:
+        for command in commands:
+            cursor.execute(command)
+
+        cursor.close()
+        con.commit()
+
+    except (Exception, psycopg2.DatabaseError) as error:
+        print(error)
+    finally:
+        if con is not None:
+            print("All records deleted")
+            con.close()
+
+
+delete_records()
