83a4591ab4c6bf4484016beff13cc7613662ff50
AdamMC-GL <60597883+AdamMC-GL@users.noreply.github.com>
2020-03-11 14:29:01 +0100

    Update data_mongo_naar_postgres.py

--- Summatieve_Opdracht2/data_mongo_naar_postgres.py
+++ Summatieve_Opdracht2/data_mongo_naar_postgres.py
@@ -1,7 +1,6 @@
 """
 Bron 1: https://stackoverflow.com/questions/13793399/passing-table-name-as-a-parameter-in-psycopg2
 Bron 2: https://www.postgresqltutorial.com/postgresql-python/insert/
-
 """
 
 import pymongo
@@ -23,7 +22,6 @@ def get_data_mongo():
     categories = set()
     brands = set()
 
-
     for i in products:
         if i["category"] is not None and i["category"] not in categories:
             categories.add(i["category"])
@@ -35,9 +33,8 @@ def get_data_mongo():
 
         insert_products_into_postgres("product", (i["_id"], i["category"], i["brand"], i["gender"], i["name"], i["price"]["selling_price"]))
 
-
     for i in profiles:
-        try:    # Try except statement wegens profielen zonder browserid
+        try:  # Try except statement wegens profielen zonder browserid
             for j in range(len(i["buids"])):
                 insert_products_into_postgres("profile", (i["buids"][j],))
         except:
@@ -68,12 +65,12 @@ def insert_products_into_postgres(table, values):
         if table == "session":
             cursor.execute("""INSERT INTO session_ VALUES(%s,%s)""", values)
 
-
         connection.commit()
         count = cursor.rowcount
-        #print(count, "Record inserted successfully into table")
+        print(count, "Record inserted successfully into table")
 
     except (Exception, psycopg2.Error) as error:
-            print("Failed to insert record into table", error)
+        print("Failed to insert record into table", error)
+
 
 get_data_mongo()
